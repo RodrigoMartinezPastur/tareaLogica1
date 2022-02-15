4f0f3424576101e8b88e1f0ad818075587222f7a
rodrigo <nikokart13@gmail.com>
2022-02-15 04:26:54 -0300

    add index.html and index.js to repository

---
+++ index.js
@@ -0,0 +1,109 @@
+// nose si las debia comentar pero las comente igual por que era molesto que me
+//saltara cada vez que hacia algo xd
+
+// Tarea 1
+
+/*
+function numeros(num1, num2, num3, num4) {
+  let num = [num1, num2, num3, num4];
+  suma = num1 + num2 + num3 + num4;
+  return console.log(suma);
+}
+
+numeros(1, 2, 3, 4);*/
+
+// tarea 2
+/*
+function pitagoras() {
+  const base = 18;
+  const altura = 15;
+  console.log((base * altura) / 2);
+  return;
+}
+
+pitagoras();
+*/
+//  Tarea 3
+
+// seguro existe otra manera de hacerlo pero solo se me ocurrio esta xd
+
+/*function años() {
+  let dias = prompt("ingrese la cantindad de dias para saber cuantos años son");
+  if (dias > 365 && dias <= 729) {
+    console.log(1);
+  } else if (dias >= 730 && dias < 2159) {
+    console.log(2);
+  } else if (dias >= 2160) {
+    console.log(6);
+  }
+}
+años();*/
+
+// tarea 4 nose si la hice bien por que decia que retorne un boleano
+/*
+function dosString(palabra, palabra2) {
+  if (palabra.length == palabra2.length) {
+    alert("true");
+  } else {
+    alert("false");
+  }
+}
+
+dosString("hola", "hola");
+*/
+
+// Tarea 5
+/*
+function darVuelta(string) {
+  return string.split("").reverse().join("");
+}
+
+console.log(darVuelta("hola"));
+*/
+
+//tarea 7
+
+/*
+function firstMayus(mayus) {
+  return mayus.charAt(0).toUpperCase() + mayus.slice(1);
+}
+
+console.log(firstMayus("buenas"));
+*/
+
+//tarea 8
+
+/*
+function elMasAlto(num1, num2, num3, num4) {
+  let array = [num1, num2, num3, num4];
+  console.log(Math.max(num1, num2, num3, num4));
+  return;
+}
+
+elMasAlto(50, 1, 35, 105);
+*/
+
+// tarea 9
+
+/*
+function firtsCharacter(first) {
+  return first.slice(1);
+}
+
+console.log(firtsCharacter("tardes"));
+*/
+
+// tarea 10
+
+/*
+function arrayToString(arr1, arr2, arr3, arr4, arr5, arr6) {
+  const laFuturaString = [arr1, arr2, arr3, arr4, arr5, arr6];
+  const strin = laFuturaString.join(" ");
+  console.log(strin);
+  return;
+}
+
+
+arrayToString("son", "las", 3, "de", "la", "mañana");
+*/
+// son las 3 de mañana y estoy en tu ventana
